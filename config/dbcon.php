43bb6ed8a1a7930742574dff3c108f8b0032038e
EmonIslamShanto <shanto35-3153@diu.edu.bd>
2023-10-30 02:51:59 +0600

    Updated

---
+++ config/dbcon.php
@@ -0,0 +1,20 @@
+<?php
+
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "e-commerce";
+
+    // Creating database connection
+    $con = mysqli_connect($host, $username, $password, $database );
+
+    // Check database connection
+    if(!$con)
+    {
+        die("Connection Failed: ". mysqli_connect_error());
+    }
+    else
+    {
+        echo "Connected Succesfully";
+    }
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include('includes/header.php');?>
+    <h1>Hello World</h1>
+
+    <button class="btn btn-primary">Testing</button>
+
+    <?php include('includes/footer.php');?>
